f21a0fdc7e8d66f91a3e37121b3f4d655be9243b
root <root@node-0.bsmat-vm.ch-geni-net.instageni.clemson.edu>
2016-06-19 16:46:21 -0400

    initializing repo

---
+++ pages/save.php
@@ -0,0 +1,35 @@
+<?php
+if (!empty($_POST))
+{
+  echo "I have request $_POST[optionsRadiosInline]";
+}
+ini_set('display_errors', 1);
+$server = 'localhost';
+$user = 'root';
+$pass = 'password';
+$conn = mysqli_connect($server,$user, $pass);
+$sql = 'CREATE Database IF NOT EXISTS test';
+mysqli_query($conn,$sql);
+mysqli_select_db($conn,'test');
+
+$sql1 = "CREATE TABLE IF NOT EXISTS data(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+Q1 VARCHAR(30) NOT NULL,
+Q2 VARCHAR(30) NOT NULL,
+Q3 VARCHAR(30) NOT NULL,
+Q4 VARCHAR(30) NOT NULL,
+Q5 VARCHAR(30) NOT NULL,
+Q6 VARCHAR(30) NOT NULL,
+Q7 VARCHAR(30) NOT NULL,
+Q8 VARCHAR(30) NOT NULL,
+Q9 VARCHAR(30) NOT NULL,
+Q10 VARCHAR(30) NOT NULL,
+Q11 VARCHAR(30) NOT NULL,
+Q12 VARCHAR(30) NOT NULL,
+Q13 VARCHAR(30) NOT NULL
+
+)";
+mysqli_query($conn,$sql1);
+mysqli_query($conn,"INSERT INTO data (Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8,Q9,Q10,Q11,Q12,Q13) VALUES ('$_POST[optionsRadiosInline]','$_POST[optionsRadiosInline2]','$_POST[optionsRadiosInline3]','$_POST[optionsRadiosInline4]','$_POST[optionsRadiosInline5]','$_POST[optionsRadiosInline6]','$_POST[optionsRadiosInline7]','$_POST[optionsRadiosInline8]','$_POST[optionsRadiosInline9]','$_POST[optionsRadiosInline10]','$_POST[optionsRadiosInline11]','$_POST[optionsRadiosInline12]','$_POST[optionsRadiosInline13]')");
+mysqli_close($conn);
+?>
