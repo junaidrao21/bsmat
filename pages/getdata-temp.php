dd30c1dab125972beac783f63161c496e4735fa5
Junaid <junaid.rao21@gmail.com>
2016-08-17 12:49:30 -0400

    Added correct getdata temp file

--- pages/getdata-temp.php
+++ pages/getdata-temp.php
@@ -94,17 +94,12 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Survey Responses</h1>
+                    <h1 class="page-header">Tables</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
             <div class="row">
-				<label> Please select the survey to view responses </label>
-				<select class="form-control">
-					<option>PAM</option>
-					<option>SDSCA</option>
-				</select>
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
